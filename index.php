837d5bd4ec4857a2cae3df474f9f9c69f2aa0c2b
Minel <Minel@example.com>
2024-01-14 13:42:34 +0100

    correction home et index

--- index.php
+++ index.php
@@ -1,26 +1,8 @@
-<?php get_header();?>
-<body>
+<?php 
+/*
+Template Name: Mes-Articles
+*/
+get_header();?>
 
-    <?php
-    // insere un short code dans la page
-    echo do_shortcode ('[products category="vedette" limit="1"]');
-    ?>
-
-<h6>Nos Vêtements</h6>
-<hr class="product-line_purple" />
-<hr class="product-line_purple2" />
-
-    <?php
-    echo do_shortcode('[products category="vetements" limit="-1"]');
-    ?>
-
-<h6>Nos Accessoires</h6>
-<hr class="product-line_purple" />
-<hr class="product-line_purple2" />
-
-    <?php
-    echo do_shortcode('[products category="accessoires" limit="-1"]');
-    ?>
-</body>
 
 <?php get_footer();?>
